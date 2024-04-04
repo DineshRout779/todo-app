843c3a41b5179f1f53223dce37474ab8b5d92ff5
Dinesh Rout <rddinesh779@gmail.com>
2024-04-04 11:15:13 +0530

    feat: adding task ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type Priority = 'p1' | 'p2' | 'p3';
 
 type Task = {
@@ -8,17 +10,42 @@ type Task = {
 };
 
 const App = () => {
-  const tasks: Task[] = [
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([
     {
       id: 1,
       title: 'Learn React',
       isCompleted: true,
     },
-  ];
+  ]);
+
+  const handleAddTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(),
+        title: task,
+        isCompleted: false,
+      },
+    ]);
+  };
 
   return (
     <div>
       <h1>Tasks</h1>
+      <form onSubmit={handleAddTask}>
+        <input
+          type="text"
+          id="todo"
+          placeholder="Add a todo"
+          name="todo"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>{task.title}</li>
