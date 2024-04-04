dbd2c9a1b196ae051f5c5bcf59c605fe26f1e5a1
Dinesh Rout <rddinesh779@gmail.com>
2024-04-04 11:51:21 +0530

    tests: adding tasks ✅

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { waitFor, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 // describe ==> group tests
@@ -14,5 +15,21 @@ describe('App', () => {
 
     expect(input).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+
+  test('should add task to list when add button is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const input = screen.getByRole('textbox', { name: 'Add Task:' });
+    const button = screen.getByRole('button', { name: 'Add' });
+
+    await user.type(input, 'New Task');
+    await user.click(button);
+
+    await waitFor(() => {
+      expect(screen.getByText('New Task')).toBeInTheDocument();
+    });
   });
 });
