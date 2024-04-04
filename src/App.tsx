c91ab3e936112ce9767f8eaa4081e77a176975d6
Dinesh Rout <rddinesh779@gmail.com>
2024-04-04 11:04:51 +0530

    feat: reading tasks ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+type Priority = 'p1' | 'p2' | 'p3';
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
+
 const App = () => {
-  return <div>Hello World!</div>;
+  const tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Learn React',
+      isCompleted: true,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
